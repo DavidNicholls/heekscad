9da58e5f7d92487f61cd79a1aefc01e58c6782bf
David Nicholls <David@Nicholls.homeip.net>
2011-11-18 14:22:06 +1100

    Just playing to figure out how to use git.

--- src/Edge.cpp
+++ src/Edge.cpp
@@ -1,6 +1,7 @@
 // Edge.cpp
 // Copyright (c) 2009, Dan Heeks
 // This program is released under the BSD license. See the file COPYING for details.
+
 #include "stdafx.h"
 #include "Edge.h"
 #include "Face.h"
